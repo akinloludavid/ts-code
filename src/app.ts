fb818410711c44ada5ebe0a4750c50b5add3c393
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2022-11-08 06:09:35 +0100

    removed morgan

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import bookRoutes from "./routes/bookRoutes";
 import bookPages from "./routes/bookPages";
-import morgan from "morgan";
 import path from "path";
 
 const app = express();
 app.use(express.json());
 
-app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.set("views", path.resolve(__dirname, "../src/views"));
